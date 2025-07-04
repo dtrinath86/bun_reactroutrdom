203655eb0dd2d15d7438d5fe81936657b36da99d
dtrinath86 <dtrinathdalai@gmail.com>
2025-07-04 23:18:46 +0530

    Initial Env

--- src/api/GetApiData.jsx
+++ src/api/GetApiData.jsx
@@ -2,7 +2,7 @@ export const GetMovieData = async () => {
     try {
 
         // const response = await fetch("https://www.omdbapi.com/?s=batman&apikey=3eb0022e");
-        const response = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=3eb0022e&s=titanic&page=1");
+        const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_APIMOVIE_KEY}&s=titanic&page=1`);
         const data = await response.json();
         return data;
         
